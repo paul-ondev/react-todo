d0e5ccbd6485790629e603763db3a84068f8b3e9
paul-ondev <p.kormanovskiy@gmail.com>
2021-06-04 18:10:00 +0300

    popup component is done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import DB from "./assets/db.json";
 import listIcon from "./assets/img/listIcon.svg";
 
 function App() {
+  const [lists, setLists] = useState(
+    DB.lists.map((item) => {
+      item.color = DB.colors.filter(
+        (color) => color.id === item.colorId
+      )[0].name;
+      return item;
+    })
+  );
+
+  const onAddList = (obj) => {
+    const newList = [...lists, obj];
+    setLists(newList);
+  };
   return (
     <div className="todo">
       <div className="todo__sidebar">
@@ -17,32 +30,8 @@ function App() {
             },
           ]}
         />
-        <List
-          items={[
-            {
-              color: "green",
-              name: "Покупки",
-            },
-            {
-              color: "blue",
-              name: "Фронтенд",
-            },
-            {
-              color: "pink",
-              name: "Фильмы и сериалы",
-            },
-            {
-              color: "lime",
-              name: "Книги",
-            },
-            {
-              color: "grey",
-              name: "Личное",
-            },
-          ]}
-          isRemovable
-        />
-        <AddList colors={DB.colors} />
+        <List items={lists} isRemovable />
+        <AddList onAdd={onAddList} colors={DB.colors} />
       </div>
       <div className="todo__tasks"></div>
     </div>
--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -5,10 +5,26 @@ import Badge from '../Badge/index';
 import addIcon from "../../assets/img/add.svg";
 import closeIcon from '../../assets/img/close.svg'
 
-function AddList({ colors }) {
+function AddList({ colors, onAdd }) {
     const [visiblePopup, setVisiblePopup] = useState(false);
     const [selectedColor, setColor] = useState(colors[0].id);
+    const [inputValue, setinputValue] = useState('');
 
+    const onClose = () => {
+        setVisiblePopup(false);
+        setinputValue('');
+        setColor(colors[0].id);
+    }
+
+    const addList = () => {
+        if (!inputValue) {
+            alert('List should has a name');
+            return;
+        }
+        const color = colors.filter(c => c.id === selectedColor)[0].name;
+        onAdd({id: Math.random(), name: inputValue, color,});
+        onClose();
+    }
 
     return(
         <div className="add-list">
@@ -24,8 +40,8 @@ function AddList({ colors }) {
             />
             {visiblePopup && (
             <div className="add-list__popup">
-                <img onClick={() => setVisiblePopup(false)} className="add-list__popup-close-btn" src={closeIcon} alt="Close Button"/>
-                <input className="field" type="text" placeholder="Название списка" />
+                <img onClick={onClose} className="add-list__popup-close-btn" src={closeIcon} alt="Close Button"/>
+                <input onChange={e => setinputValue(e.target.value)} value={inputValue} className="field" type="text" placeholder="Название списка" />
                 <div className="add-list__popup-colors">
                     
                     {
@@ -33,7 +49,7 @@ function AddList({ colors }) {
                     }
                     
                 </div>
-                <button className="button">Добавить</button>
+                <button onClick={addList}  className="button">Добавить</button>
             </div>)}
         </div>
             
